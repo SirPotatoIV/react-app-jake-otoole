cf2edfeb7566c7e51669ccee8c0b46c1bbbf07d6
SirPotatoIV <otoolej33@gmail.com>
2020-05-05 21:24:25 -0500

    Added file to fix error in Netlify deployment.

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Form,
+  FormField,
+  Heading,
+  Main,
+  TextInput,
+} from "grommet";
 
 function About() {
-  return <h2>About</h2>;
+  const [value, setValue] = useState({});
+
+  return (
+    <Main>
+      <Heading>About</Heading>
+      <Form
+        value={value}
+        onChange={(nextValue) => setValue(nextValue)}
+        onReset={() => setValue({})}
+        onSubmit={({ value }) => {}}
+      >
+        <FormField name="name" htmlfor="text-input-id" label="Name">
+          <TextInput id="text-input-id" name="name" />
+        </FormField>
+        <Box direction="row" gap="medium">
+          <Button type="submit" primary label="Submit" />
+          <Button type="reset" label="Reset" />
+        </Box>
+      </Form>
+    </Main>
+  );
 }
 
 export default About;
