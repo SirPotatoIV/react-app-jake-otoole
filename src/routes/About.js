3ce39b6780e4e2c2261ee540196b1ee3202d7401
SirPotatoIV <otoolej33@gmail.com>
2020-05-06 13:21:34 -0500

    Switched About back to content in my React template.

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,36 +1,7 @@
-import React, { useState } from "react";
-import {
-  Box,
-  Button,
-  Form,
-  FormField,
-  Heading,
-  Main,
-  TextInput,
-} from "grommet";
+import React from "react";
 
 function About() {
-  const [value, setValue] = useState({});
-
-  return (
-    <Main>
-      <Heading>About</Heading>
-      <Form
-        value={value}
-        onChange={(nextValue) => setValue(nextValue)}
-        onReset={() => setValue({})}
-        onSubmit={({ value }) => {}}
-      >
-        <FormField name="name" htmlfor="text-input-id" label="Name">
-          <TextInput id="text-input-id" name="name" />
-        </FormField>
-        <Box direction="row" gap="medium">
-          <Button type="submit" primary label="Submit" />
-          <Button type="reset" label="Reset" />
-        </Box>
-      </Form>
-    </Main>
-  );
+  return <h2>About</h2>;
 }
 
 export default About;
